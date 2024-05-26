2cfade43517e6b69fe27209a0bf3f96390fa64f9
hol16046 <hol16046@byui.edu>
2024-05-25 22:32:35 -0600

    Add New Product Imag

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostename: 'wdd430-group.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
