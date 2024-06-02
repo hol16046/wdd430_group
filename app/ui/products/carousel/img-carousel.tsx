a95230373f58d8bc0366fb91cf56c9017c27e7f0
hol16046 <hol16046@byui.edu>
2024-06-01 21:51:05 -0600

    Add Image Carousel

--- app/ui/products/carousel/img-carousel.tsx
+++ app/ui/products/carousel/img-carousel.tsx
@@ -2,6 +2,9 @@ import { table } from 'console';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { SelectProductImage } from '@/app/lib/definitions';
+import { useState } from 'react';
+import { ArrowBigLeft, ArrowBigRight, Circle, CircleDot } from 'lucide-react';
+import Image from 'next/image';
 
 // import * as React from 'react';
 // import { ButtonGroupProps, ArrowProps, DotProps } from 'react-multi-carousel/lib/types';
@@ -50,27 +53,66 @@ const responsive = {
   }
 };
 
-export default function ImgCarousel({ images } { images: SelectProductImage[] }) {
-  
-  return(
-    <Carousel
-    swipeable={false}
-    draggable={false}
-    showDots={true}
-    responsive={responsive}
-    ssr={true} // means to render carousel on server-side.
-    infinite={true}
-    keyBoardControl={true}
-    customTransition='all .5'
-    transitionDuration={500}
-    containerClass='carousel-container'
-    removeArrowOnDeviceType={["tablet", "mobile"]}
-    deviceType={this.props.deviceType}
-    dotListClass='custom-dot-list-style'
-    itemClass='carousel-item-padding-40-px'
-    partialVisbile={true}
-    >
+export default function ImgCarousel({ images }: { images: SelectProductImage[] }) {
+  const [imageIndex, setImageIndex] = useState(0);
 
-    </Carousel>
-  );
+  function showPrevImage() {
+    setImageIndex(index => {
+      if (index === 0) return images.length - 1;
+      return index - 1;
+    })
+  }
+
+  function showNextImage() {
+    setImageIndex(index => {
+      if (index === images.length - 1) return 0;
+      return index + 1;
+    })
+  }
+
+  return(
+    <div aria-label='Image Carousel' className='w-full h-full relative mb-2 '>
+      <a href='#after-carousel-controls' className='sr-only focus:not-sr-only'>Skip to after image carousel controls</a>
+      <div className='w-full h-full overflow-hidden flex'>
+        {images.map((image, index) => (
+          <Image style={{ translate: `${-100 * imageIndex}%`}} key={image.image_file} src={image.image_file} alt={image.alt_text} aria-hidden={imageIndex !== index} width={350} height={250} className='w-full h-full object-cover block flex-shrink-0 flex-grow-0 translate ease-in-out duration-300'/>
+        ))}
+      </div>
+      
+      <button onClick={showPrevImage} className='carousel-btn left-0' aria-label='View Previous Image'><ArrowBigLeft aria-hidden /></button>
+      <button onClick={showNextImage} className='carousel-btn right-0' aria-label='View Next Image'><ArrowBigRight aria-hidden /></button>
+      <div className='absolute bottom-[.5rem] left-[50%] -translate-x-[50%] flex gap-[.25rem]'>
+        {images.map((_, index) => (
+          <button onClick={() => setImageIndex(index)} key={index} className='carousel-dot-btn' aria-label={`View Image ${index + 1}`}>{index === imageIndex ? <CircleDot aria-hidden /> : <Circle aria-hidden />}</button>
+        ))}
+      </div>
+      <div id='after-carousel-controls' className='sr-only'>After Image Carousel Controls</div>
+    </div>
+  )
+
+
+  // style={{ translate: `${-100 * imageIndex}%`}}
+  // className='-translate-x-[{100%*${imageIndex}}]'
+
+  // return(
+  //   <Carousel
+  //   swipeable={false}
+  //   draggable={false}
+  //   showDots={true}
+  //   responsive={responsive}
+  //   ssr={true} // means to render carousel on server-side.
+  //   infinite={true}
+  //   keyBoardControl={true}
+  //   customTransition='all .5'
+  //   transitionDuration={500}
+  //   containerClass='carousel-container'
+  //   removeArrowOnDeviceType={["tablet", "mobile"]}
+  //   deviceType={this.props.deviceType}
+  //   dotListClass='custom-dot-list-style'
+  //   itemClass='carousel-item-padding-40-px'
+  //   partialVisbile={true}
+  //   >
+
+  //   </Carousel>
+  // );
 };
--- app/ui/products/large-product.tsx
+++ app/ui/products/large-product.tsx
@@ -2,6 +2,7 @@
 
 import { SelectProduct, SelectProductImage } from '../../lib/definitions';
 import Image from 'next/image';
+import ImgCarousel from './carousel/img-carousel';
 
 
 
@@ -24,12 +25,13 @@ export default function LargeProduct({ product, images }: { product: SelectProdu
   return (
     <div className="grid grid-cols-3 px-3" >
       <h2 className="p-3 col-span-3 font-playfair text-2xl font-semibold mx-auto">{product.name}</h2>
-      <Image className="rounded-md justify-self-center col-span-3 mb-3" src={imageFile} width={350} height={250} alt={altText}/>
+      {/* <Image className="rounded-md justify-self-center col-span-3 mb-3" src={imageFile} width={350} height={250} alt={altText}/> */}
+      <div className='col-span-3 mb-3'>
+        <ImgCarousel images={images} />
+      </div>
       <p className="text-theme-dark-teal text-md font-medium self-center p-1">${product.price}</p>
       <button type="button" className="focus:outline-none text-white bg-theme-dark-teal hover:bg-theme-rust focus:ring-4 focus:ring-theme-orange font-medium rounded-md text-sm px-5 py-2.5 col-span-2 self-center justify-self-end">Add to Cart</button>
       <p className="text-md p-3 col-span-3">{product.description}</p>
     </div>
   );
 }
-
-// I want to have a slideshow of images for the product if possible
