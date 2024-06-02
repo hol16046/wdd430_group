75cf4bae11801ffb7df2437a0a1af4e82e789b0a
hol16046 <hol16046@byui.edu>
2024-06-01 22:21:49 -0600

    Fix Metadata Vercel Error

--- app/seller_img/upload/page.tsx
+++ app/seller_img/upload/page.tsx
@@ -1,9 +1,5 @@
-'use client';
- 
-import { type PutBlobResult } from '@vercel/blob';
-import { upload } from '@vercel/blob/client';
-import { useState, useRef } from 'react';
 import { Metadata } from "next";
+import SellerImageUploadForm from '@/app/ui/artists/seller-img-upload';
 
 export const metadata: Metadata = {
     title: 'Upload Images',
@@ -12,38 +8,9 @@ export const metadata: Metadata = {
 
  
 export default function SellerImageUploadPage() {
-  const inputFileRef = useRef<HTMLInputElement>(null);
-  const [blob, setBlob] = useState<PutBlobResult | null>(null);
   return (
     <main className="font-red-hat">
-      <h1>Upload New Seller Shop Image</h1>
- 
-      <form
-        onSubmit={async (event) => {
-          event.preventDefault();
- 
-          if (!inputFileRef.current?.files) {
-            throw new Error('No file selected');
-          }
- 
-          const file = inputFileRef.current.files[0];
- 
-          const newBlob = await upload(file.name, file, {
-            access: 'public',
-            handleUploadUrl: '/api/avatar/upload',
-          });
- 
-          setBlob(newBlob);
-        }}
-      >
-        <input name="file" ref={inputFileRef} type="file" required />
-        <button type="submit">Upload</button>
-      </form>
-      {blob && (
-        <div>
-          Blob url: <a href={blob.url}>{blob.url}</a>
-        </div>
-      )}
+      <SellerImageUploadForm />
     </main>
   );
 }
