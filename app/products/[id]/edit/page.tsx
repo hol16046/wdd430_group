859009e98723308214cf2d09ecc1fbc1d667d954
hol16046 <hol16046@byui.edu>
2024-06-04 13:39:55 -0600

    fix edit product page

--- app/products/[id]/edit/page.tsx
+++ app/products/[id]/edit/page.tsx
@@ -1,4 +1,4 @@
-import { fetchSellerById, fetchProductById } from '@/app/lib/data';
+import { fetchSellerById, fetchProductData } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 import EditProductForm from '@/app/ui/products/edit-product-form';
 import Header from '@/app/ui/header/header';
@@ -9,7 +9,7 @@ export default async function EditProductPage({ params }: { params: { id: number
   const id = params.id;
   const [seller, product] = await Promise.all([
     fetchSellerById(2),
-    fetchProductById(id),
+    fetchProductData(id),
   ]);
 
   if (!product || !seller) {
