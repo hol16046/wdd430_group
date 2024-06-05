9e11d26996c2be618b99a0185fe0380ce37340c2
hol16046 <hol16046@byui.edu>
2024-06-05 12:57:14 -0600

    testing without profile content

--- app/profile/(overview)/page.tsx
+++ app/profile/(overview)/page.tsx
@@ -6,7 +6,6 @@ import  { EditProfile } from '@/app/ui/seller/buttons';
 import { Metadata } from "next";
 import { SelectUser } from '@/app/lib/definitions';
 import { SessionContext, SessionProvider, useSession } from 'next-auth/react';
-import PageContent from '../pagecontent';
 import { auth } from '@/auth';
 import Head from 'next/head';
 import { fetchUserData } from '@/app/lib/data';
@@ -36,11 +35,11 @@ export default async function Page() {
                 <h2>Welcome {(await users).f_name} {(await users).l_name}</h2>
                 <h3>{(await users).email}</h3>
 
-                <EditProfile id={id} />
+                {/* <EditProfile id={id} /> */}
             </div>
             <div className='container grid grid-cols-1 lg:grid-cols-5 gap-4 mx-auto w-full p-4'>
-                {/* @ts-expect-error Server Component */}
-                <SellerProductsWrapper />
+                
+                {/* <SellerProductsWrapper /> */}
             </div>
             <Footer />
 
