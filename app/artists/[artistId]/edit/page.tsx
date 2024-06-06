089e7d1be9606dc19a2089bcd034fdbb6ce9e082
hol16046 <hol16046@byui.edu>
2024-06-05 18:23:45 -0600

    fix edit profile

--- app/artists/[artistId]/edit/page.tsx
+++ app/artists/[artistId]/edit/page.tsx
@@ -20,23 +20,23 @@ export default async function EditProductPage({ params }: { params: { artistId: 
     notFound();
   }
 
-    if (!session || !session?.user || user_id !== sellerId) {
-        return (
-            <main className='mx-auto font-red-hat'>
-                <Header/>
-                <div className='grid mx-auto py-5 h-[100vw] sm:h-[65vw] md:h-[30vw] w-[80%]'>
-                    <div className='col-span-full self-center justify-self-center font-semibold text-center text-2xl text-theme-rust'>Sorry, you are not authorized to view this page.</div>
-                    <Link
-                        href={`/profile`}
-                        className='form-btn col-span-full mt-2 self-center justify-self-center'>
-                        Back to Profile
-                    </Link>
-                </div>
-                <Footer />
+  if (!session || !session?.user || user_id !== sellerId) {
+      return (
+          <main className='mx-auto font-red-hat'>
+              <Header/>
+              <div className='grid mx-auto py-5 h-[100vw] sm:h-[65vw] md:h-[30vw] w-[80%]'>
+                  <div className='col-span-full self-center justify-self-center font-semibold text-center text-2xl text-theme-rust'>Sorry, you are not authorized to view this page.</div>
+                  <Link
+                      href={`/profile`}
+                      className='form-btn col-span-full mt-2 self-center justify-self-center'>
+                      Back to Profile
+                  </Link>
+              </div>
+              <Footer />
 
-            </main>
-        );
-    }
+          </main>
+      );
+  }
 
   return (
     <main className="font-red-hat">
